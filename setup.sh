44f29be7edeca81613f238f8fc14aee540a36d7d
Korben Kirscht <kirscht@gmail.com>
2021-02-21 20:01:05 -0800

    Initial

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ which git || brew install git
 which VBoxManage || brew cask install virtualbox
 which vagrant || brew cask install vagrant
 stat /Applications/Cyberduck.app || brew cask install cyberduck
-#brew cask install intellij-idea-ce
-brew cask install goland
+brew cask install intellij-idea-ce
+stat /Applications/GoLand.app || brew cask install goland
