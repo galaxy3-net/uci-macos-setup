488e72f4494738bc06f162f0ee31ef064929a313
Korben Kirscht <kirscht@gmail.com>
2021-02-21 21:26:43 -0800

    Initial

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+brew update || git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask" fetch --unshallow
 brew update
 which code || brew install --cask visual-studio-code
 which python3 || brew install python3
 which git || brew install git
 which VBoxManage || brew install --cask virtualbox
 which vagrant || brew install --cask vagrant
-stat /Applications/Cyberduck.app || brew install --cask cyberduck
+stat "/Applications/Cyberduck.app" || brew install --cask cyberduck
 stat "/Applications/IntelliJ IDEA CE.app" || brew  install --cask intellij-idea-ce
 stat "/Applications/GoLand.app" || brew install --cask goland
-stat /Applications/iTerm.app || brew install --cask iterm2
+stat "/Applications/iTerm.app" || brew install --cask iterm2
