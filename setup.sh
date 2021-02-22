ba6f159b2df138b0855ed19dc7dceef12cf513f2
Korben Kirscht <kirscht@gmail.com>
2021-02-21 21:22:27 -0800

    Initial

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ which VBoxManage || brew install --cask virtualbox
 which vagrant || brew install --cask vagrant
 stat /Applications/Cyberduck.app || brew install --cask cyberduck
 stat "/Applications/IntelliJ IDEA CE.app" || brew  install --cask intellij-idea-ce
-stat /Applications/GoLand.app || brew install --cask goland
+stat "/Applications/GoLand.app" || brew install --cask goland
 stat /Applications/iTerm.app || brew install --cask iterm2
