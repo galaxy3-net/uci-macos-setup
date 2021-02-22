c60b4fbfd743965e7dd36a2c6255583086d0fba1
Korben Kirscht <kirscht@gmail.com>
2021-02-21 19:56:14 -0800

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # uci-macos-setup
+Setting up MacOS for UCI Cybersecurity Bootcamp
+
+## Install Base Packages
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo bash -c "$(curl -s )"
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+brew update
+which code || brew cask install visual-studio-code
+which python3 || brew install python3
+which git || brew install git
+which VBoxManage || brew cask install virtualbox
+which vagrant || brew cask install vagrant
+stat /Applications/Cyberduck.app || brew cask install cyberduck
+#brew cask install intellij-idea-ce
+brew cask install goland
